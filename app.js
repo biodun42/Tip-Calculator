b3fb0ab90d2eddaae1134b2633adc0a3f76bba19
Muhammed Abiodun <muhammedabiodun42@gmail.com>
2024-05-04 09:03:23 +0100

    commit

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ function ifZero() {
   if (billInput.value === "") {
     theBill.classList.add("active");
     billInputm.classList.add("active");
-    document.getElementById("tip").innerText = "0.00";
-    document.getElementById("Total").innerText = "0.00";
+    document.getElementById("tip").innerText = "$0.00";
+    document.getElementById("Total").innerText = "$0.00";
   } else {
     theBill.classList.remove("active");
     billInputm.classList.remove("active");
@@ -20,8 +20,8 @@ function ifZero() {
   if (numPeopleInput.value === "") {
     theBilltwo.classList.add("active");
     billinputn.classList.add("active");
-    document.getElementById("tip").innerText = "0.00";
-    document.getElementById("Total").innerText = "0.00";
+    document.getElementById("tip").innerText = "$0.00";
+    document.getElementById("Total").innerText = "$0.00";
   } else {
     theBilltwo.classList.remove("active");
     billinputn.classList.remove("active");
@@ -35,58 +35,56 @@ function calculateTipOne() {
   const tipPerPerson = tip / numPeople;
   const totalPerPerson = tipPerPerson * numPeople;
 
-  document.getElementById("tip").innerText = tipPerPerson.toFixed(2);
-  document.getElementById("Total").innerText = totalPerPerson.toFixed(2);
+  document.getElementById("tip").innerText = "$" + tipPerPerson.toFixed(2);
+  document.getElementById("Total").innerText = "$" + totalPerPerson.toFixed(2);
   ifZero();
 }
 
 function calculateTipTwo() {
   const bill = parseFloat(billInput.value);
   const numPeople = parseInt(numPeopleInput.value);
-
   const tip = bill * 0.1;
   const tipPerPerson = tip / numPeople;
   const totalPerPerson = tipPerPerson * numPeople;
 
-  document.getElementById("tip").innerText = tipPerPerson.toFixed(2);
-  document.getElementById("Total").innerText = totalPerPerson.toFixed(2);
+  document.getElementById("tip").innerText = "$" + tipPerPerson.toFixed(2);
+  document.getElementById("Total").innerText = "$" + totalPerPerson.toFixed(2);
   ifZero();
 }
 
 function calculateTipThree() {
   const bill = parseFloat(billInput.value);
   const numPeople = parseInt(numPeopleInput.value);
-
   const tip = bill * 0.15;
   const tipPerPerson = tip / numPeople;
   const totalPerPerson = tipPerPerson * numPeople;
-  document.getElementById("tip").innerText = tipPerPerson.toFixed(2);
-  document.getElementById("Total").innerText = totalPerPerson.toFixed(2);
+
+  document.getElementById("tip").innerText = "$" + tipPerPerson.toFixed(2);
+  document.getElementById("Total").innerText = "$" + totalPerPerson.toFixed(2);
   ifZero();
 }
 
 function calculateTipFour() {
   const bill = parseFloat(billInput.value);
   const numPeople = parseInt(numPeopleInput.value);
-
   const tip = bill * 0.25;
   const tipPerPerson = tip / numPeople;
   const totalPerPerson = tipPerPerson * numPeople;
-  document.getElementById("tip").innerText = tipPerPerson.toFixed(2);
-  document.getElementById("Total").innerText = totalPerPerson.toFixed(2);
+
+  document.getElementById("tip").innerText = "$" + tipPerPerson.toFixed(2);
+  document.getElementById("Total").innerText = "$" + totalPerPerson.toFixed(2);
   ifZero();
 }
 
 function calculateTipFive() {
   const bill = parseFloat(billInput.value);
   const numPeople = parseInt(numPeopleInput.value);
-
   const tip = bill * 0.5;
   const tipPerPerson = tip / numPeople;
   const totalPerPerson = tipPerPerson * numPeople;
 
-  document.getElementById("tip").innerText = tipPerPerson.toFixed(2);
-  document.getElementById("Total").innerText = totalPerPerson.toFixed(2);
+  document.getElementById("tip").innerText = "$" + tipPerPerson.toFixed(2);
+  document.getElementById("Total").innerText = "$" + totalPerPerson.toFixed(2);
   ifZero();
 }
 
@@ -94,21 +92,20 @@ customTip.addEventListener("input", function () {
   const bill = parseFloat(billInput.value);
   const customTips = parseFloat(customTip.value / 100);
   const numPeople = parseInt(numPeopleInput.value);
-
   const tip = bill * customTips;
   const tipPerPerson = tip / numPeople;
   const totalPerPerson = tipPerPerson * numPeople;
 
-  document.getElementById("tip").innerText = tipPerPerson.toFixed(2);
-  document.getElementById("Total").innerText = totalPerPerson.toFixed(2);
+  document.getElementById("tip").innerText = "$" + tipPerPerson.toFixed(2);
+  document.getElementById("Total").innerText = "$" + totalPerPerson.toFixed(2);
   ifZero();
 });
 
 function reset() {
   billInput.value = "";
   numPeopleInput.value = "";
-  document.getElementById("tip").innerText = "0.00";
-  document.getElementById("Total").innerText = "0.00";
+  document.getElementById("tip").innerText = "$0.00";
+  document.getElementById("Total").innerText = "$0.00";
   theBill.classList.remove("active");
   billInputm.classList.remove("active");
   theBilltwo.classList.remove("active");
